6d6ea3f7857198e414ed4a78f7dcb7e763a9b16a
Kamila <kmilaporto1991@gmail.com>
2021-03-04 11:16:29 -0300

    ajusta pacotes

--- src/br/com/alura/gerenciador/servlet/EditaEmpresaServlet.java
+++ src/br/com/alura/gerenciador/servlet/EditaEmpresaServlet.java
@@ -11,6 +11,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import br.com.alura.gerenciador.modelo.Banco;
+import br.com.alura.gerenciador.modelo.Empresa;
+
 @WebServlet("/editaEmpresa")
 public class EditaEmpresaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
